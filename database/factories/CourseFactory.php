e5d4769de62b96cb2a9844dba12c1ac65b3bfde8
Carlos Ferreira <conta@especializati.com.br>
2021-08-11 16:32:09 -0300

    Create Factories

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CourseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Course::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->sentence(10),
+        ];
+    }
+}
---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LessonFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Lesson::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->fake->unique()->name();
+
+        return [
+            'module_id' => Module::factory(),
+            'name' => $name,
+            'video' => $name,
+            'description' => $this->faker->sentence(10),
+        ];
+    }
+}
